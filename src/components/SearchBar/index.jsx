e3210291b7892c5e0b2a276895731d3bc990139f
Rishu <kumarrishu571@gmail.com>
2023-11-08 22:16:11 +0530

    frontend completed

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className='flex gap-4 my-10 justify-center px-10'>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Job Role</option>
+            <option value="ios Developer">ios Developer</option>
+            <option value="Frontend Developer">Frontend Developer</option>
+            <option value="Backend Developer">Backend Developer</option>
+            <option value="Android Developer">Android Developer</option>
+            <option value="Developer Advocate">Developer Advocate</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Job Type</option>
+            <option value="Full Time">Full Time</option>
+            <option value="Part Time">Part Time</option>
+            <option value="Contract">Contract</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Location</option>
+            <option value="Remote">Remote</option>
+            <option value="In-Office">In-Office</option>
+            <option value="Hybrid">Hybrid</option>
+        </select>
+        <select className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
+            <option value="" disabled hidden selected>Experience</option>
+            <option value="ios Developer">Fresher</option>
+            <option value="Frontend Developer">Junior Level</option>
+            <option value="Backend Developer">Mid Level</option>
+            <option value="Android Developer">Senior Level</option>
+        </select>
+        <button className='w-64 bg-blue-500 text-white font-bold py-3 rounded-md'>Search</button>
+    </div>
+  )
+}
+
+export default SearchBar
